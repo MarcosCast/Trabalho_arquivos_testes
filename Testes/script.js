b6d68ed1a1ded982d540715d6adc3283ce7c6b50
Marcos Castro <marcosmax.5@gmail.com>
2023-08-17 12:06:59 -0300

    Ajustando página para testar tooltip

--- Testes/script.js
+++ Testes/script.js
@@ -1,19 +1,29 @@
 var chart = new chart({
-    renderTo: "chart",
-    type: "line",
-    data: {
-      labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-      datasets: [
-        {
-          data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: "Gráfico"
-      }
-    }
-  });
+  renderTo: "chart",
+  type: "line",
+  data: {
+    labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+    datasets: [
+      {
+        data: [10, 20,
+
+
+// var chart = new chart({
+//     renderTo: "chart",
+//     type: "line",
+//     data: {
+//       labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+//       datasets: [
+//         {
+//           data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
+//         }
+//       ]
+//     },
+//     options: {
+//       title: {
+//         display: true,
+//         text: "Gráfico"
+//       }
+//     }
+//   });
   
